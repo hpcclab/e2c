866daa0355b06f622c69d0055a9461bce8527205
jryan4902 <--global>
2026-04-14 22:50:08 -0500

    IOT Cosmetic Preset with icons

---
+++ client/src/utils/iotIcons.js
@@ -0,0 +1,21 @@
+import {
+  MdVideocam,
+  MdThermostat,
+  MdLightbulb,
+  MdLock,
+  MdSpeaker,
+  MdSmartphone,
+  MdTv,
+  MdDirectionsCar,
+} from "react-icons/md";
+
+export const IOT_ICON_MAP = {
+  MdVideocam,
+  MdThermostat,
+  MdLightbulb,
+  MdLock,
+  MdSpeaker,
+  MdSmartphone,
+  MdTv,
+  MdDirectionsCar,
+};
